b2f4a11f5d184150486de08a04fed0894d748018
Ryush <seunghyun87.ryu@gmail.com>
2022-12-02 23:53:49 +0900

    작업 중

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,6 @@
 
 'use strict'
-
+//clock
 function getClock() {
   const clockBx = document.querySelector(".clock-bx");
   const date = new Date();
@@ -10,8 +10,10 @@ function getClock() {
   clockBx.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+//login
 const loginForm = document.querySelector("#loginForm");
 const loginInput = document.querySelector("#loginForm input");
+const greeting = document.querySelector("#greeting");
 const todoBx = document.querySelector(".todo-bx");
 
 const HIDDEN_CLASSNAME = "hidden";
@@ -26,7 +28,8 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  todoBx.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello~! ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
   todoBx.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -39,6 +42,94 @@ if (savedUsername === null) {
   paintGreetings(savedUsername);
 }
 
+//toDoList
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector(".todo-bx");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
+// background Image
+const images = ["_RYU1428.jpg", "2N1A3240.jpg", "2N1A7330.jpg", "2N1A7409.jpg", "2N1A7502.jpg", "20190825_200238.jpg", "DSCF6358.jpg", "IMG_0852.jpg", "IMG_9633.jpg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+document.body.style.backgroundImage=`url(${bgImage.src})`;
+
+// weather
+const weather = document.querySelector(".weather-bx span:first-child");
+const city = document.querySelector(".weather-bx span:last-child");
+const API_KEY = "817e72f4e02c583c99e9115c2bfd5bb0";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+// common
 function docInit(){
   getClock();
   setInterval(getClock, 1000);
